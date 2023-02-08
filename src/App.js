901aac80626e45d2aa289edccb44e9f9f28a2f13
Vasylyna Lykhogodenko <vasilisapj89@gmail.com>
2023-02-08 13:07:44 +0200

    feat: Added SignIn component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component.jsx";
 import Navigation from "./routes/navigation/navigation.component.jsx";
+import SignIn from "./routes/sign-in/sign-in.component.jsx";
 
 const Shop = () => {
   return <h3>I am shop component!</h3>;
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/" element={<Navigation />}>
           <Route index element={<Home />} />
           <Route path="shop" element={<Shop />} />
+          <Route path="sign-in" element={<SignIn />} />
         </Route>
       </Routes>
     </>
