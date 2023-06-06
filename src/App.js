40f60f39feae72c47e44c5a27573533436167428
Vasylyna Lykhogodenko <107197536+codingmemories@users.noreply.github.com>
2023-06-06 23:38:54 +0300

    feat: update with comment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
+// components
 import Home from "./routes/home/home.component.jsx";
 import Navigation from "./routes/navigation/navigation.component.jsx";
 import SignIn from "./routes/sign-in/sign-in.component.jsx";
